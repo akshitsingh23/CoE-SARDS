a36ff1adecf99e0ecd0c75e246d3a76196a390f8
AKSHIT SINGH <akshitsingh2352003@gmail.com>
2024-12-18 00:11:49 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      {/* <img src={logo} className="h-16 w-16" alt="logo" /> */}
+    </div>
+  );
+}
+
+export default App;
