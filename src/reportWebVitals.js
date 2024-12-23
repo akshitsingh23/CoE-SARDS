d4b0855d684623ce3e46f8f7d0a0fc7044ca8e08
AKSHIT SINGH <akshitsingh2352003@gmail.com>
2024-12-23 21:42:31 +0530

    routing problemas

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,10 @@
+/**
+ * @typedef {import('web-vitals').ReportHandler} ReportHandler
+ */
+
+/**
+ * @param {ReportHandler} onPerfEntry
+ */
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
