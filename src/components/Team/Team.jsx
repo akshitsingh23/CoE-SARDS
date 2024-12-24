96c2668de6f7781eeac51ef96f80913f1c20e23c
AKSHIT SINGH <akshitsingh2352003@gmail.com>
2024-12-24 16:23:44 +0530

    Team added

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -5,6 +5,8 @@ import './Team.css';
 import colonel from './colonel.JPG';
 import general from './general.jpg';
 import atharva from './atharva.png';
+import harbans from './harbans.jpg';
+import admiral from './admiral.jpg';
 // import img2 from '../global-images/'
 // import temp from '../'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -32,31 +34,24 @@ const teamMembers = [
     designation: 'Professor Of Practice',
     linkedin: 'https://www.linkedin.com/in/colprabirsengupta/',
   },
+  {
+    imgSrc:admiral,
+    name: 'Read Adm. Brijesh Vashishth',
+    designation: 'Professor Of Practice',
+    linkedin: 'https://www.linkedin.com/in/',
+  },
+  {
+    imgSrc: harbans,
+    name: 'Harbans Singh (Veteran)',
+    designation: 'Sr. Executive Officer',
+    linkedin: '/',
+  }
 ];
 
 function Team() {
   return (
     <div className='flex justify-center items-center h-auto bg-black'>
       <div className="container mx-5 flex-col">
-        {/* <div className=''>
-          <h1 className='text-center bg-clip-text text-transparent mb-5 bg-gradient-to-br from-purple-700 to-green-600 text-center mt-5 font-bold text-5xl'>Advisors</h1>
-        </div>
-        <div className='flex justify-center items-center'>
-          <div className="grid xl:grid-cols-2  md:grid-cols-2 gap-3 flex-wrap">
-            {advisor.map((member, index) => ( 
-              <div key={index} className="outTeam__card hover:scale-90 hover:text-orange-200 transition duration-200 ease-in-out hover:bg-gradient-to-tr from-slate-900 to-green-500">
-                <div className='grid grid-cols-1 justify-items-center'>
-                  <img src={member.imgSrc} alt={member.name} className="outTeam__cardImg fa-border border-green-400 border-4" />
-                  <div className='p-2 font-montserrat font-bold  rounded  text-center'>{member.name}</div>
-                  <div className=' font-montserrat rounded  text-center'>{member.designation}</div>
-                  <a href={member.linkedin} target="_blank" rel="noopener noreferrer" >
-                    <FontAwesomeIcon icon={faLinkedin} className='p-5 ' size="3x" color="#0077B5" />
-                  </a>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div> */}
         <br />
         <br />
         <br />
