3aede9bd6719085166f21bbb99de08b5e7447154
AKSHIT SINGH <akshitsingh2352003@gmail.com>
2025-01-03 13:04:39 +0530

    team better layout

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -48,7 +48,7 @@
 //     designation: 'Adjunct Faculty',
 //     linkedin: 'https://www.linkedin.com/in/colprabirsengupta/',
 //   },
-  
+
 //   {
 //     imgSrc: harbans,
 //     name: 'Harbans Bharadwaj (Veteran)',
@@ -127,6 +127,7 @@ const teamMembers = {
       designation: 'Sr. Executive Officer',
       linkedin: '/',
     },
+    
   ],
   professorsOfPractice: [
     {
@@ -152,97 +153,98 @@ const teamMembers = {
 
 function Team() {
   return (
-    <div
-      className="flex justify-center items-center h-auto bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-white py-10"
-    >
+    <div className="flex justify-center items-center h-auto bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-white py-10">
       <div className="container mx-5 flex-col">
         <br />
         <br />
         <br />
         {/* <h1 className="text-center bg-clip-text text-transparent mb-5 bg-gradient-to-tr from-red-400 to-blue-300 text-center mt-5 font-montserrat font-bold text-5xl">
           Dedicated Team
-        </h1> */}
+          </h1> */}
         <div className="grid lg:grid-cols-2 gap-10">
-          {/* Administration Section */}
-          <div className='flex flex-col items-center '>
-            <h2 className="text-center text-orange-400 font-montserrat font-bold text-4xl mb-5">
-              Administrative
-            </h2>
-            <div className="grid sm:grid-cols-2 gap-5 ">
-              {teamMembers.administration.map((member, index) => (
-                <div
-                  key={index}
-                  className="outTeam__card hover:scale-105 hover:text-orange-200 transition duration-200 ease-in-out bg-gradient-to-br from-gray-900 to-gray-700 shadow-lg rounded-lg p-5"
-                >
-                  <div className="flex flex-col justify-between h-full">
-                    <div className="grid grid-cols-1 justify-items-center">
-                      <img
-                        src={member.imgSrc}
-                        alt={member.name}
-                        className="outTeam__cardImg fa-border border-orange-400 border-4 rounded-full"
-                      />
-                      <div className="mt-3 p-2 font-montserrat font-bold rounded text-center text-lg">
-                        {member.name}
-                      </div>
-                      <div className="font-montserrat rounded text-center text-gray-400">
-                        {member.designation}
+          <div className="grid  gap-10">
+            {/* Administration Section */}
+            <div className='flex flex-col items-center '>
+              <h2 className="text-center text-orange-400 font-montserrat font-bold text-4xl mb-5">
+                Administrative
+              </h2>
+              <div className="grid sm:grid-cols-2 gap-5">
+                {teamMembers.administration.map((member, index) => {
+                  const isLastElement = index === teamMembers.administration.length - 1;
+                  const isOdd = teamMembers.administration.length % 2 !== 0;
+                  return (
+                    <div
+                      key={index}
+                      className={`outTeam__card hover:scale-105 hover:text-orange-200 transition duration-200 ease-in-out bg-gradient-to-br from-gray-900 to-gray-700 shadow-lg rounded-lg p-5 ${isLastElement && isOdd ? 'md:col-span-2 md:justify-self-center lg:col-span-2 lg:justify-self-center' : ''}`}
+                    >
+                      <div className="flex flex-col justify-between h-full">
+                        <div className="grid grid-cols-1 justify-items-center">
+                          <img
+                            src={member.imgSrc}
+                            alt={member.name}
+                            className="outTeam__cardImg fa-border border-yellow-500 border-4"
+                          />
+                          <div className='p-2 font-montserrat font-bold rounded text-center'>{member.name}</div>
+                          <div className='font-montserrat rounded text-center'>{member.designation}</div>
+                        </div>
+                        <div className='flex justify-center'>
+                          <Link to={member.linkedin} target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faLinkedin} className='p-5' size="3x" color="#0077B5" />
+                          </Link>
+                        </div>
                       </div>
                     </div>
-                    <div className="flex justify-center mt-3">
-                      <Link to={member.linkedin} target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon
-                          icon={faLinkedin}
-                          className="p-5 hover:text-blue-300"
-                          size="2x"
-                          color="#0077B9"
-                        />
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              ))}
+                  );
+                })}
+              </div>
             </div>
           </div>
-
-          {/* Professors of Practice Section */}
-          <div className='flex flex-col items-center'>
-            <h2 className="text-center text-blue-400 font-montserrat font-bold text-4xl mb-5">
-              Professors of Practice
-            </h2>
-            <div className="grid sm:grid-cols-2 gap-5">
-              {teamMembers.professorsOfPractice.map((member, index) => (
-                <div
-                  key={index}
-                  className="outTeam__card hover:scale-105 hover:text-blue-200 transition duration-200 ease-in-out bg-gradient-to-br from-gray-800 to-gray-700 shadow-lg rounded-lg p-5"
-                >
-                  <div className="flex flex-col justify-between h-full">
-                    <div className="grid grid-cols-1 justify-items-center">
-                      <img
-                        src={member.imgSrc}
-                        alt={member.name}
-                        className="outTeam__cardImg fa-border border-blue-400 border-4 rounded-full"
-                      />
-                      <div className="mt-3 p-2 font-montserrat font-bold rounded text-center text-lg">
-                        {member.name}
-                      </div>
-                      <div className="font-montserrat rounded text-center text-gray-400">
-                        {member.designation}
+          <div className="grid gap-10 ">
+            {/* Professors of Practice Section */}
+            <div className='flex flex-col items-center '>
+              <h2 className="text-center text-blue-400 font-montserrat font-bold text-4xl mb-5">
+                Professors of Practice
+              </h2>
+              <div className="grid sm:grid-cols-2 gap-5">
+                {teamMembers.professorsOfPractice.map((member, index) => {
+                  const isLastElement = index === teamMembers.professorsOfPractice.length - 1;
+                  const isOdd = teamMembers.professorsOfPractice.length % 2 !== 0;
+                  return (
+                    <div
+                      key={index}
+                      className={`outTeam__card hover:scale-105 hover:text-blue-200 transition duration-200 ease-in-out bg-gradient-to-br from-gray-800 to-gray-700 shadow-lg rounded-lg p-5 ${isLastElement && isOdd ? 'md:col-span-2 md:justify-self-center lg:col-span-2 lg:justify-self-center' : ''}`}
+                    >
+                      <div className="flex flex-col justify-between h-full">
+                        <div className="grid grid-cols-1 justify-items-center">
+                          <img
+                            src={member.imgSrc}
+                            alt={member.name}
+                            className="outTeam__cardImg fa-border border-blue-400 border-4 rounded-full"
+                          />
+                          <div className="mt-3 p-2 font-montserrat font-bold rounded text-center text-lg">
+                            {member.name}
+                          </div>
+                          <div className="font-montserrat rounded text-center text-gray-400">
+                            {member.designation}
+                          </div>
+                        </div>
+                        <div className="flex justify-center mt-3">
+                          <Link to={member.linkedin} target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon
+                              icon={faLinkedin}
+                              className="p-5"
+                              size="2x"
+                              color="#0077B5"
+                            />
+                          </Link>
+                        </div>
                       </div>
                     </div>
-                    <div className="flex justify-center mt-3">
-                      <Link to={member.linkedin} target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon
-                          icon={faLinkedin}
-                          className="p-5"
-                          size="2x"
-                          color="#0077B5"
-                        />
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              ))}
+                  );
+                })}
+              </div>
             </div>
+
           </div>
         </div>
       </div>
